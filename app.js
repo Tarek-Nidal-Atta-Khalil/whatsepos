8bf8c32266aa063dd8bb8313fc5c2edc59e6db95
Tarek-Nidal-Atta-Khalil <t.khalil@stud.uni-goettingen.de>
2026-05-30 22:53:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {
   erstelleAnalysezeile,
   pruefeVersVorlaeufig,
   setzeFormaeMetricas
-} from "./hexameter_flex.js?v=20260530-crossword-merge-1";
+} from "./hexameter_flex.js?v=20260530-cache-reset-1";
 
 const sideMenu = document.getElementById("sideMenu");
 const menuButton = document.getElementById("menuButton");
