cc344b858fdc1f779090674548dd6172cf19dd72
Tarek-Nidal-Atta-Khalil <t.khalil@stud.uni-goettingen.de>
2026-06-07 16:08:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {
   erstelleAnalysezeile,
   pruefeVersVorlaeufig,
   setzeFormaeMetricas
-} from "./hexameter.js?v=20260607-liaisones-1";
+} from "./hexameter.js?v=20260607-liaisones-2";
 
 const sideMenu = document.getElementById("sideMenu");
 const menuButton = document.getElementById("menuButton");
